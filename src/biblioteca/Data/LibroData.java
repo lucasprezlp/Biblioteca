a8e9cc80355ee22881222be26b9d9ee6b53c14db
llucaspprez <lucasprez@hotmail.com>
2023-10-02 17:58:20 -0300

    4

--- src/biblioteca/Data/LibroData.java
+++ src/biblioteca/Data/LibroData.java
@@ -10,5 +10,5 @@ package biblioteca.Data;
  * @author lucas
  */
 public class LibroData {
-    
+   
 }
--- src/biblioteca/Entidades/Libro.java
+++ src/biblioteca/Entidades/Libro.java
@@ -10,21 +10,12 @@ public class Libro {
    public String tipo;
    public String Editor;
    public boolean estado;
+   public int numEjemplares;
 
     public Libro() {
     }
 
-    public Libro(int isbn, String titulo, Autor autor, int anio, String tipo, String Editor, boolean estado) {
-        this.isbn = isbn;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.anio = anio;
-        this.tipo = tipo;
-        this.Editor = Editor;
-        this.estado = estado;
-    }
-
-    public Libro(int idLibro, int isbn, String titulo, Autor autor, int anio, String tipo, String Editor, boolean estado) {
+    public Libro(int idLibro, int isbn, String titulo, Autor autor, int anio, String tipo, String Editor, boolean estado, int numEjemplares) {
         this.idLibro = idLibro;
         this.isbn = isbn;
         this.titulo = titulo;
@@ -33,61 +24,93 @@ public class Libro {
         this.tipo = tipo;
         this.Editor = Editor;
         this.estado = estado;
+        this.numEjemplares = numEjemplares;
+    }
+
+    public Libro(int isbn, String titulo, Autor autor, int anio, String tipo, String Editor, boolean estado, int numEjemplares) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.tipo = tipo;
+        this.Editor = Editor;
+        this.estado = estado;
+        this.numEjemplares = numEjemplares;
     }
 
     public int getIdLibro() {
         return idLibro;
     }
+
     public void setIdLibro(int idLibro) {
         this.idLibro = idLibro;
     }
+
     public int getIsbn() {
         return isbn;
     }
+
     public void setIsbn(int isbn) {
         this.isbn = isbn;
     }
+
     public String getTitulo() {
         return titulo;
     }
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
     public Autor getAutor() {
         return autor;
     }
+
     public void setAutor(Autor autor) {
         this.autor = autor;
     }
+
     public int getAnio() {
         return anio;
     }
+
     public void setAnio(int anio) {
         this.anio = anio;
     }
+
     public String getTipo() {
         return tipo;
     }
+
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
+
     public String getEditor() {
         return Editor;
     }
+
     public void setEditor(String Editor) {
         this.Editor = Editor;
     }
+
     public boolean isEstado() {
         return estado;
     }
+
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
 
-    @Override
-    public String toString() {
-        return "Libro{" + "idLibro=" + idLibro + ", isbn=" + isbn + ", titulo=" + titulo + ", autor=" + autor + ", anio=" + anio + ", tipo=" + tipo + ", Editor=" + Editor + ", estado=" + estado + '}';
+    public int getNumEjemplares() {
+        return numEjemplares;
     }
+
+    public void setNumEjemplares(int numEjemplares) {
+        this.numEjemplares = numEjemplares;
+    }
+
+
    
    
    
