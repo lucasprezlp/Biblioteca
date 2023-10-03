f68824c8fdbb6d754586cea2513493850b7eb6ef
llucaspprez <lucasprez@hotmail.com>
2023-10-03 18:41:50 -0300

    6

--- src/biblioteca/Data/LectorData.java
+++ src/biblioteca/Data/LectorData.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package biblioteca.Data;
 
-/**
- *
- * @author lucas
- */
+import biblioteca.Entidades.Lector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
 public class LectorData {
+    private Connection con=null;
+    
+    public LectorData(){
+        con=conexion.getConexion();
+    }
+    
+    
+    
+    public void activarLector(int idLector){
+        String sql = "UPDATE lector SET estado = 1 WHERE idLector=?";
+         
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idLector);
+            int exito=ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null, "Se activo el lector");
+            }
+   
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la BD lector");        }
+        
+    }
+    
+       public void desactivarLector(int idLector){
+        String sql = "UPDATE lector SET estado = 0 WHERE idLector=?";
+         
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idLector);
+            int exito=ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null, "Se DESactivo el lector");
+            }
+   
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la BD lector");        }
+        
+    } 
+    
+    
     
 }
