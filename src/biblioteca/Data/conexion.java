25e7657e716625ba5c1ccaf8bc02e2197d56ae66
VeroHerLabarden <verochernandez1130@gmail.com>
2023-10-03 18:33:56 -0300

    Vistas

--- src/biblioteca/Data/conexion.java
+++ src/biblioteca/Data/conexion.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package biblioteca.Data;
 
-/**
- *
- * @author lucas
- */
+import java.sql.*;
+
 public class conexion {
-    
+
+    private static final String url = "jdbc:mariadb://localhost/";
+    private static final String bd = "universidad";
+    private static final String usuario = "root";
+    private static final String pass = "";
+
+    private static Connection conn;
+
+    private conexion() {
+    }
+
+    public static Connection getConexion() {
+        if (conn == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conn = DriverManager.getConnection(url + bd + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                        + "&user=" + usuario + "&password=" + pass);
+
+                //  System.out.println("Conexion Exitosa");
+            } catch (ClassNotFoundException | SQLException ex) {
+                System.out.println("No se ha podido conectar" + ex);
+            }
+        }
+        return conn;
+    }
+
+    public void desconectar() {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("No se ha podido desconectar");
+        }
+    }
 }
